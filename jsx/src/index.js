39c1023e255be9a5e3a4f3365198696492b13834
joneshshrestha <contact@joneshshrestha.com>
2019-12-02 09:40:29 +0545

    update

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Noodles</li>
-      <li>Cheese</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <div>
+//     <h1>My Favorite Foods</h1>
+//     <ul>
+//       <li>Bacon</li>
+//       <li>Noodles</li>
+//       <li>Cheese</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+let name = "Jonesh";
+ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
