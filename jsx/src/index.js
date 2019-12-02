36472ddba28378f3dae87ca1dd3ac6c16fd1bc8d
joneshshrestha <contact@joneshshrestha.com>
2019-12-02 09:55:18 +0545

    update

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -13,12 +13,12 @@ import ReactDOM from "react-dom";
 //   document.getElementById("root")
 // );
 
-let name = "Jonesh";
-let luckyNumber = 10;
+let fname = "Jonesh";
+let lname = "Shrestha";
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
-    <p>Your lucky number is {luckyNumber}</p>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
