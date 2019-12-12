3db094c9943241753487db5609a7d7ab14f8f28f
joneshshrestha <contact@joneshshrestha.com>
2019-12-12 14:55:23 +0545

    update

--- jsx/src/greeting.js
+++ jsx/src/greeting.js
@@ -1,16 +1,11 @@
 //Create a React app from scratch.
-//Show a single h1 that says "Good morning" if between midnight and 12PM.
-//or "Good Afternoon" if between 12PM and 6PM.
-//or "Good evening" if between 6PM and midnight.
-//Apply the "heading" style in the styles.css
-//Dynamically change the color of the h1 using inline css styles.
-//Morning = red, Afternoon = green, Night = blue.
-
 import React from "react";
 import ReactDOM from "react-dom";
 
 let date = new Date();
 let currentHour = date.getHours();
+let greeting = greetingFunc();
+let heading;
 
 function greetingFunc() {
   if (currentHour >= 24 && currentHour <= 12) {
@@ -22,11 +17,30 @@ function greetingFunc() {
   }
 }
 
-let greeting = greetingFunc();
-
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+if (greeting === "Morning") {
+  heading = {
+    color: "red"
+  };
+} else if (greeting === "Afternoon") {
+  heading = {
+    color: "green"
+  };
+} else if (greeting === "Night") {
+  heading = {
+    color: "blue"
+  };
+}
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
 ReactDOM.render(
   <div>
-    <h1 className="heading">Good {greeting}</h1>
+    <h1 className="heading" style={heading}>
+      Good {greeting}
+    </h1>
   </div>,
   document.getElementById("root")
 );
