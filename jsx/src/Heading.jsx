57178e43fcc08fb79ee963ae8046c55ddba71987
joneshshrestha <contact@joneshshrestha.com>
2019-12-14 21:45:42 +0545

    update

--- jsx/src/Heading.jsx
+++ jsx/src/Heading.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 function Heading() {
   return <h1>My Favorite Foods</h1>;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
+import App from "./App";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // let fname = "Jonesh";
 // let lname = "Shrestha";
