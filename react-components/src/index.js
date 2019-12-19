921763ced0aef989d5c0ba38509204142bc74aea
joneshshrestha <contact@joneshshrestha.com>
2019-12-19 14:37:12 +0545

    update

--- react-components/src/index.js
+++ react-components/src/index.js
@@ -2,24 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const date = new Date();
-const currentTime = date.getHours();
-
-let greeting;
-
-const customStyle = {
-  color: ""
-};
-
-if (currentTime < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Night";
-  customStyle.color = "blue";
-}
-
 ReactDOM.render(<App />, document.getElementById("root"));
