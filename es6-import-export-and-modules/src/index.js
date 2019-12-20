3f473a8b63dc225de3562d5c9d1b303b56dd89a1
joneshshrestha <contact@joneshshrestha.com>
2019-12-20 09:28:21 +0545

    update

--- es6-import-export-and-modules/src/index.js
+++ es6-import-export-and-modules/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+//import * as pi from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
