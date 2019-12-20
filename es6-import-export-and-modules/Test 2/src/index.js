a11acf23dfb9782333349731e7406e9679d5a106
joneshshrestha <contact@joneshshrestha.com>
2019-12-20 09:33:21 +0545

    update

--- es6-import-export-and-modules/Test 2/src/index.js
+++ es6-import-export-and-modules/Test 2/src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as calc from "./calculator";
 
-//Import the add, multiply, subtract and divide functions
-//from the calculator.js file.
-//If successful, your website should look the same as the Final.png
+//Import the add, multiply, subtract and divide functions from the calculator.js file.
 
 ReactDOM.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
+    <li>{calc.add(1, 2)}</li>
+    <li>{calc.multiply(2, 3)}</li>
+    <li>{calc.subtract(7, 2)}</li>
+    <li>{calc.divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
