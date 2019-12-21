9b29994ab4c4999cfe8b553bbabca064ce4d177a
joneshshrestha <contact@joneshshrestha.com>
2019-12-21 17:09:35 +0545

    update

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,2 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
--- keeper-app/src/components/Footer.jsx
+++ keeper-app/src/components/Footer.jsx
@@ -1,2 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+let date = new Date();
+let year = date.getFullYear();
+
+function Footer() {
+  return <p>copyright {year} Jonesh</p>;
+}
