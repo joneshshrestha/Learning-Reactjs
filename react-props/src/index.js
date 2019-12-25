aa61984d56a07f78fc2c847af6a70e284067419b
joneshshrestha <contact@joneshshrestha.com>
2019-12-25 15:32:07 +0545

    update

--- react-props/src/index.js
+++ react-props/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom";
 function Card(props) {
   return (
     <div>
-      <h2>{props.img}</h2>
-      <img src={props.img} alt="avatar_img" />
+      <h2>{props.name}</h2>
+      <img src={props.img} />
       <p>{props.tel}</p>
       <p>{props.email}</p>
     </div>
