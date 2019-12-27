6c78bfd5d0346491460779fa024f7cc6bb7d9046
joneshshrestha <contact@joneshshrestha.com>
2019-12-27 08:41:55 +0545

    update

--- react-props/react-props-practice/src/index.js
+++ react-props/react-props-practice/src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Contacts</h1>
+    <App
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
 
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
 //2. Extract the contact card as a reusable Card component.
 //3. Use props to render the default Beyonce contact card
 //so the Card component can be reused for other contacts.
